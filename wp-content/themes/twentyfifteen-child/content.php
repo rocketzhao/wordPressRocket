0078ef622eecd70066fead56c4d38199a30f1387
zhaojun <727507531@qq.com>
2015-10-23 14:05:53 +0800

    添加广告信息，添加2015子主题，删除自带的wordpress官方link

---
+++ wp-content/themes/twentyfifteen-child/content.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * The default template for displaying content
+ *
+ * Used for both single and index/archive/search.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fifteen
+ * @since Twenty Fifteen 1.0
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php
+		// Post thumbnail.
+		twentyfifteen_post_thumbnail();
+	?>
+
+	<header class="entry-header">
+		<?php
+			if ( is_single() ) :
+				the_title( '<h1 class="entry-title">', '</h1>' );
+			else :
+				the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+			endif;
+		?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php
+			/* translators: %s: Name of current post */
+			the_content( sprintf(
+				__( 'Continue reading %s', 'twentyfifteen' ),
+				the_title( '<span class="screen-reader-text">', '</span>', false )
+			) );
+
+			wp_link_pages( array(
+				'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentyfifteen' ) . '</span>',
+				'after'       => '</div>',
+				'link_before' => '<span>',
+				'link_after'  => '</span>',
+				'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'twentyfifteen' ) . ' </span>%',
+				'separator'   => '<span class="screen-reader-text">, </span>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<?php
+		// Author bio.
+		if ( is_single() && get_the_author_meta( 'description' ) ) :
+			get_template_part( 'author-bio' );
+		endif;
+	?>
+
+	<footer class="entry-footer">
+		<?php twentyfifteen_entry_meta(); ?>
+		<?php edit_post_link( __( 'Edit', 'twentyfifteen' ), '<span class="edit-link">', '</span>' ); ?>
+	</footer><!-- .entry-footer -->
+
+</article><!-- #post-## -->
+	
+<div id="footer-sidebar" class="secondary">
+	<div id="footer-sidebar1">
+		<?php
+		if(is_active_sidebar('footer-sidebar-1')){
+			dynamic_sidebar('footer-sidebar-1');
+		}
+		?>
+	</div>
+	<div id="footer-sidebar2">
+		<?php
+		if(is_active_sidebar('footer-sidebar-2')){
+			dynamic_sidebar('footer-sidebar-2');
+		}
+		?>
+	</div>
+</div>
---
+++ wp-content/themes/twentyfifteen-child/functions.php
@@ -0,0 +1,44 @@
+<?php
+
+//Import parent styles the right way and add other stylesheets if necessary.
+function twenty_fifteen_child_styles() {
+	wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+}
+add_action( 'wp_enqueue_scripts', 'twenty_fifteen_child_styles' );
+
+
+/**
+ * Register footer widget area.
+ *
+ * @since Twenty Fifteen Child 1.0
+ *
+ * @link https://codex.wordpress.org/Function_Reference/register_sidebar
+ */
+function twentyfifteen_child_widgets_init() {
+	register_sidebar( array(
+		'name' => 'Footer Sidebar 1',
+		'id' => 'footer-sidebar-1',
+		'description' => 'Appears in the footer area',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => '</aside>',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
+	
+	register_sidebar( array(
+		'name' => 'Footer Sidebar 2',
+		'id' => 'footer-sidebar-2',
+		'description' => 'Appears in the footer area',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => '</aside>',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
+	
+}
+add_action( 'widgets_init', 'twentyfifteen_child_widgets_init' );
+
+
+
+
+?>
